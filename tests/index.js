df0f038ee158d3a726d89cbe7fb6c3dcdb42bbc0
Michael Ekpang <michaelekpang@Michaels-MacBook-Pro.local>
2018-11-29 11:55:55 +0100

    chore(tests): implement unit testing
    - update index.js file
    - change functions to arrow functions on test file
    
    [Finishes #161779858]

--- tests/index.js
+++ tests/index.js
@@ -3,8 +3,8 @@ import chai, { expect } from "chai";
 import server from "../index";
 
 chai.use(chaiHttp);
-describe("#sample test", function() {
-  it("should return hello world", function() {
+describe("#sample test", () => {
+  it("should return hello world", () => {
     const test = "hello world";
     expect(test).to.equal("hello world");
   });
