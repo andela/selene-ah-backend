a910e0be2427cbd4cbfc7a38263edc2ca2e8d48d
Esther Falayi <esther.falayi@andela.com>
2018-11-29 13:56:14 +0100

    fix(travis-ci): fix failing test
    - update eslint rules
    - update package.lock
    - fix lint errors
    
    [#161779858]

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ if (!isProduction) {
   app.use(errorhandler());
 }
 
-app.use(require("./routes"));
+app.use(require('./routes'));
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error("Not Found");
+// / catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
