df8913b4e88b2258bf0dba7f6044b048d9c2d470
Michael Ekpang <weezyval93@gmail.com>
2018-12-03 00:31:02 +0100

    feat(models): create comment expression model - create the comment expression file [Delivers #162342447]

--- config/config.js
+++ config/config.js
@@ -1,23 +1,25 @@
-import dotenv from 'dotenv';
+require('babel-core/register');
+const dotenv = require('dotenv');
+
 
 dotenv.config();
 
-export default {
+module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DEV,
     host: process.env.DB_HOST,
-    dialect: process.env.DIALECT,
+    dialect: process.env.DIALECT
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_TEST,
     host: process.env.DB_HOST,
-    dialect: process.env.DIALECT,
+    dialect: process.env.DIALECT
   },
   production: {
-    DB_URL: process.env.DATABASE_URL,
+    DB_URL: process.env.DATABASE_URL
   }
 };
