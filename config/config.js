099cb25cbb804b4f1bc68c94e7aafa73c091ed3e
gbols <gbolahanpaul@gmail.com>
2018-12-03 02:10:25 +0100

    model for articles created [finishes #162342273]

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
-import dotenv from 'dotenv';
+require('babel-core/register');
+const dotenv = require('dotenv');
 
 dotenv.config();
-
-export default {
+module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
