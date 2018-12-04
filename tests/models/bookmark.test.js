89ee5dcf0aa78b75957154c6609e52659ed394b2
Daniel Shotonwa <danielshotonwa53@gmail.com>
2018-12-04 13:21:39 +0100

    feat(test): implement test for bookmark model
    - create a test file for testing the models
    - install sequelize-test-helpers and sinon for testing
    
    [Delivers #162382971]

---
+++ tests/models/bookmark.test.js
@@ -0,0 +1,33 @@
+import { sequelize, dataTypes } from 'sequelize-test-helpers';
+import BookmarkModel from '../../server/models/bookmark';
+import UserModel from '../../server/models/user';
+import ArticleModel from '../../server/models/articles';
+import { expect } from 'chai';
+
+describe('models/bookmark', () => {
+  const Bookmark = BookmarkModel(sequelize, dataTypes);
+  const User = UserModel(sequelize, dataTypes);
+  const Article = ArticleModel(sequelize, dataTypes);
+  const bookmark = new Bookmark();
+
+  before(() => {
+    Bookmark.associate({ User })
+    Bookmark.associate({ Article })
+  });
+
+  it('should have a valid model name ', () => {       
+    expect(Bookmark.modelName).to.equal('Bookmark')
+  });
+  
+  it('should have property id', () => {
+    expect(bookmark).to.have.property('id')
+  });
+
+  it('should have an association with UserModel', () => {
+    expect(Bookmark.belongsTo.calledWith(User)).to.equal(true);
+  });
+
+  it('should have an association with ArticleModel', () => {
+    expect(Bookmark.belongsTo.calledWith(Article)).to.equal(true);
+  });
+});
