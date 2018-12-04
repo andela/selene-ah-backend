8c5907c94847fc8f8caffa41a2a7b716e12418bb
Esther Falayi <esther.falayi@andela.com>
2018-12-04 18:55:19 +0100

    chore(lint): fix lint issue

--- tests/models/commenthistory.test.js
+++ tests/models/commenthistory.test.js
@@ -1,47 +1,46 @@
-import CommentHistoryModel from '../../server/models/commenthistory';
-import Comment from '../../server/models/comment';
 import {
-	expect
+  expect
 } from 'chai';
 import {
-	sequelize,
-	dataTypes,
+  sequelize,
+  dataTypes,
 } from 'sequelize-test-helpers';
+import CommentHistoryModel from '../../server/models/commenthistory';
+import Comment from '../../server/models/comment';
 
 describe('CommentHistory Model', () => {
-	const CommentHistory = CommentHistoryModel(sequelize, dataTypes);
-	const commentHistory = new CommentHistory();
+  const CommentHistory = CommentHistoryModel(sequelize, dataTypes);
+  const commentHistory = new CommentHistory();
 
-	context('Check if the CommentHistory model exists', () => {
-		it('should have model valid model name (CommentHistory) ', () => {
-			expect(CommentHistory.modelName).to.equal('CommentHistory');
-		})
-	})
+  context('Check if the CommentHistory model exists', () => {
+    it('should have model valid model name (CommentHistory) ', () => {
+      expect(CommentHistory.modelName).to.equal('CommentHistory');
+    });
+  });
 
-	context('Check the properties of the User Model', () => {
-		it('The CommentHistory model should have the property "id"', () => {
-			expect(commentHistory).to.have.property('id');
-		})
+  context('Check the properties of the User Model', () => {
+    it('The CommentHistory model should have the property "id"', () => {
+      expect(commentHistory).to.have.property('id');
+    });
 
-		it('The CommentHistory model should have the property "commentId"', () => {
-			expect(commentHistory).to.have.property('commentId');
-		})
+    it('The CommentHistory model should have the property "commentId"', () => {
+      expect(commentHistory).to.have.property('commentId');
+    });
 
-		it('The CommentHistory model should have the property "comment_history"', () => {
-			expect(commentHistory).to.have.property('comment_history');
-		})
-	})
+    it('The CommentHistory model should have the property "comment_history"', () => {
+      expect(commentHistory).to.have.property('comment_history');
+    });
+  });
 
-	context('Check the CommentHistory Model associations', () => {
-		before(() => {
-			CommentHistory.associate({
-				Comment
-			})
-		});
+  context('Check the CommentHistory Model associations', () => {
+    before(() => {
+      CommentHistory.associate({
+        Comment
+      });
+    });
 
-		it('The CommentHistory model has a many-to-one association with the Comment Model as "comhis"', () => {
-			expect(CommentHistory.belongsTo.calledWith(Comment)).to.equal(true);
-		});
-
-	});
+    it('The CommentHistory model has a many-to-one association with the Comment Model as "comhis"', () => {
+      expect(CommentHistory.belongsTo.calledWith(Comment)).to.equal(true);
+    });
+  });
 });
