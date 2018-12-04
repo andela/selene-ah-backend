34305a8e050f7ef2552e5d3e5f9207fa937a6a7c
Idris Kelani <idriskelani@Idriss-MacBook-Pro.local>
2018-12-04 16:25:25 +0100

    feat(notification model): create notification model
    - generate notification  models and migration
    - reorganise notifications model and migration
    - write test for model
    
    [Dilivers #162385388]

---
+++ server/migrations/20181204145930-create-notification.js
@@ -0,0 +1,51 @@
+
+const notification = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Notifications', {
+    id: {
+      allowNull: false,
+      type: Sequelize.UUID,
+      primaryKey: true,
+      default: Sequelize.UUIDV4
+    },
+    isSeen: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    },
+    notificationType: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    contentTypeId: {
+      type: Sequelize.UUID,
+      allowNull: false
+    },
+    contentType: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    userId: {
+      type: Sequelize.UUID,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    ownerId: {
+      type: Sequelize.UUID,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+
+  down: queryInterface => queryInterface.dropTable('Notifications')
+};
+
+export default notification;
---
+++ server/models/notification.js
@@ -0,0 +1,44 @@
+/**
+ * Represent notifications model
+ * @param {object} sequelize
+ * @param {object} DataTypes
+ * @returns {object} - notification
+ */
+const notification = (sequelize, DataTypes) => {
+  const Notification = sequelize.define('Notification', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    isSeen: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    notificationType: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    contentType: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    contentTypeId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    ownerId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    }
+  });
+  Notification.associate = (models) => {
+    Notification.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
+  };
+  return Notification;
+};
+
+export default notification;
